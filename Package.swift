1e197c4a6f1ac27dc5a73ce15607aab0cfa0f0af
Meysam Farmani <farmani.meysam2@gmail.com>
2024-04-18 19:40:12 +0330

    New GizoSDK version 1.0.0

--- Package.swift
+++ Package.swift
@@ -1,30 +1,42 @@
-// swift-tools-version: 5.8
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
+let version = "1.0.0"
+let moduleName = "GizoSDK"
+let checksum = ""
+
 let package = Package(
-    name: "GizoSDK",
-    platforms: [
-        .iOS(.v13)
-    ],
+    name: "GizoSDK-iOS",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v13)],
     products: [
         .library(
-            name: "GizoSDK",
-            targets: ["GizoSDK"]),
+            name: "GizoSDK-iOS",
+            targets: ["GizoSDK-iOS"]),
     ],
     dependencies: [
         .package(name: "MapboxMaps", url: "https://github.com/mapbox/mapbox-maps-ios.git", .exact("10.12.3")),
         .package(name: "MapboxNavigation", url: "https://github.com/mapbox/mapbox-navigation-ios.git", .exact("2.12.0"))
     ],
     targets: [
+        .binaryTarget(
+            name: moduleName,
+            url: "https://github.com/Tetromize/gizo-ios-sdk-without-ai-source/releases/download/\(version)/\(moduleName).xcframework.zip",
+            checksum: checksum
+        ),
         .target(
-            name: "GizoSDK",
-            dependencies: ["MapboxMaps", "MapboxNavigation"],
-            path: "GizoSDK"
-            )
-    ],
-    swiftLanguageVersions: [
-        .v5
+            name: "GizoSDK-iOS",
+            dependencies: ["MapboxMaps", "MapboxNavigation", "GizoSDK"],
+            resources: [.copy("Gizo.bundle")],
+            linkerSettings: [
+                .linkedLibrary("z"),
+                .linkedLibrary("bz2"),
+                .linkedLibrary("sqlite3"),
+                .linkedFramework("CoreML"),
+                .linkedFramework("SystemConfiguration")
+            ]
+        )
     ]
 )
---
+++ prepare_package.sh
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  gizo-ios-sdk-without-ai-source
+//
+//  Created by Meysam Farmani on 4/18/24.
+//
+
+import Foundation
